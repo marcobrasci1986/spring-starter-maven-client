d093e1ab92072d6eff005e8167dbeef46c52050e
marcobrasci1986 <marco_brasci@hotmail.com>
2019-04-09 23:06:20 +0200

    Working Version

--- src/main/java/be/avidoo/client/starterclient/StarterClientApplication.java
+++ src/main/java/be/avidoo/client/starterclient/StarterClientApplication.java
@@ -2,12 +2,15 @@ package be.avidoo.client.starterclient;
 
 
 import be.avidoo.kring.commons.GreetingService;
+import be.avidoo.kring.commons.config.ObjectMapperConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(ObjectMapperConfiguration.class)
 public class StarterClientApplication implements CommandLineRunner {
 
     @Autowired
