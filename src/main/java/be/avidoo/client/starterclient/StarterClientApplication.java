96b6b732ed2c9735609b447d06576b5e810926d3
marcobrasci1986 <marco_brasci@hotmail.com>
2019-04-09 23:12:02 +0200

    Working Version

--- src/main/java/be/avidoo/client/starterclient/StarterClientApplication.java
+++ src/main/java/be/avidoo/client/starterclient/StarterClientApplication.java
@@ -2,15 +2,12 @@ package be.avidoo.client.starterclient;
 
 
 import be.avidoo.kring.commons.GreetingService;
-import be.avidoo.kring.commons.config.ObjectMapperConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(ObjectMapperConfiguration.class)
 public class StarterClientApplication implements CommandLineRunner {
 
     @Autowired
@@ -20,10 +17,8 @@ public class StarterClientApplication implements CommandLineRunner {
         SpringApplication.run(StarterClientApplication.class, args);
     }
 
-
     @Override
     public void run(String... strings) throws Exception {
-
         String result = greetingService.sayHello("Lebron");
         System.out.println(result);
     }
